db920be325babb61155c05b3d362777350131bb1
arturomadrigal10 <54254566+arturomadrigal10@users.noreply.github.com>
2022-07-22 02:04:55 -0500

    CW1
    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.

---
+++ cw1.js
@@ -0,0 +1,11 @@
+/*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
+
+
+
+function boolToWord( bool ){ // function with a named of boolToWord with a parameter of bool
+  if (bool){ // check for truthy or falsey value. 
+    return 'Yes' // if bool is truthy return yes
+  }else
+    return 'No' // bool is falsey so it returns no
+  //...
+}
